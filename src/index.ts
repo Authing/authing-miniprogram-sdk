2a83304a8b6c8352bacd0965b5f00025366acc2a
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-05-25 22:58:49 +0800

    fixed lint

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,30 @@ import apiProxy from '@authing/authingmove-api-proxy'
 
 AuthingMove.use(apiProxy)
 
-const storageRes = wx
-  .setStorage({
-    key: 'setStorageKey',
-    data: {
-      a: 1,
-      b: 2,
-      c: 3,
-      d: 4
-    },
-    success: res => {
-      console.log('wx.setStorage success: ', res)
-    }
-  })
+const storageRes = wx.setStorage({
+  key: 'setStorageKey',
+  data: {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+  },
+  success: res => {
+    console.log('wx.setStorage success: ', res)
+  }
+})
 
 console.log('storageRes: ', storageRes)
 
 wx.getStorage({
   key: 'ssdf'
-}).then(res => {
-  console.log('wx.getStorage then: ', res)
-}).catch(res => {
-  console.log('wx.getStorage catch: ', res)
 })
+  .then(res => {
+    console.log('wx.getStorage then: ', res)
+  })
+  .catch(res => {
+    console.log('wx.getStorage catch: ', res)
+  })
 
 wx.scanCode({}).then(res => {
   console.log('wx.scanCode then: ', res)
